0c0a233438a2cea74eddbcc23e4bc29212662200
CarylThom <cat.twnti@gmail.com>
2022-10-24 11:54:40 +0000

    Second part deployment to heroku

--- FitSpace/settings.py
+++ FitSpace/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$-ahzr2398=xzyc_!oq)(49j7qhxn@jgwr0jrf7)iomy2%c2d0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fit-space.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,13 +118,17 @@ WSGI_APPLICATION = 'FitSpace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
