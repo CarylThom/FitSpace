c9577e90268fdd474906b321a75d495192c91382
CarylThom <cat.twnti@gmail.com>
2022-11-07 13:44:12 +0000

    Adding model to contact & correcting line length

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ Contact form fields """
+
+    name = models.CharField(max_length=75, blank=False, null=False)
+    email = models.EmailField(max_length=256, blank=False, null=False)
+    message = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.email
--- contact/views.py
+++ contact/views.py
@@ -2,34 +2,38 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib import messages
+
 from .forms import ContactForm
+from .models import Contact
 
 
 def contact(request):
     """ Contact form view """
-    contact_form = ContactForm
+    contact_form = ContactForm(request.POST or None)
     if request.method == "POST":
-        form_data = {
-            'name': request.POST.get('name', ''),
-            'email': request.POST.get('email', ''),
-            'message': request.POST.get('message', '')
-        }
-        print(f"FORM_DATA: {form_data}")
-        form = contact_form(form_data)
+        # form_data = {
+        #     'name': request.POST.get('name', ''),
+        #     'email': request.POST.get('email', ''),
+        #     'message': request.POST.get('message', '')
+        # }
+        # print(f"FORM_DATA: {form_data}")
 
-        if form.is_valid():
+        if contact_form.is_valid():
             print("Form is valid")
+            contact_form.save()
             send_mail(
-                form_data['name'] + " - " + form_data['email'],
-                form_data['message'],
-                'fitspace@example.com',
-                [settings.DEFAULT_FROM_EMAIL],
+                'Thank you for your message',
+                request.POST.get('message'),
+                settings.DEFAULT_FROM_EMAIL,
+                [request.POST.get('email')],
                 fail_silently=False,
             )
-        messages.success(
-            request, 'Thank you for your message, we aim to get back to you within 48 hours.'
-            )
-        return redirect('contact')
+            messages.success(
+                request,
+                'Thank you for your message, '
+                'we aim to get back to you within 48 hours.'
+                )
+            return redirect('contact')
     context = {
         'form': contact_form,
     }
