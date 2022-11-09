5a9219f9166b925de5d7367469706a830fb11ee6
CarylThom <cat.twnti@gmail.com>
2022-11-09 18:27:33 +0000

    Correcting line errors in python

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,10 +5,17 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
-                  'county', 'town_or_city', 'country',
-                  'postcode',)
+        fields = (
+            "full_name",
+            "email",
+            "phone_number",
+            "street_address1",
+            "street_address2",
+            "county",
+            "town_or_city",
+            "country",
+            "postcode",
+        )
 
     def __init__(self, *args, **kwargs):
         """
@@ -17,23 +24,23 @@ class OrderForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'full_name': 'Full Name',
-            'email': 'Email Address',
-            'phone_number': 'Phone Number',
-            'street_address1': 'Street Address 1',
-            'street_address2': 'Street Address 2',
-            'town_or_city': 'Town or City',
-            'county': 'County',
-            'country': 'Country',
-            'postcode': 'Postal Code',
+            "full_name": "Full Name",
+            "email": "Email Address",
+            "phone_number": "Phone Number",
+            "street_address1": "Street Address 1",
+            "street_address2": "Street Address 2",
+            "town_or_city": "Town or City",
+            "county": "County",
+            "country": "Country",
+            "postcode": "Postal Code",
         }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True
+        self.fields["full_name"].widget.attrs["autofocus"] = True
         for field in self.fields:
             if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
+                placeholder = f"{placeholders[field]} *"
             else:
                 placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].widget.attrs["placeholder"] = placeholder
+            self.fields[field].widget.attrs["class"] = "stripe-style-input"
             self.fields[field].label = False
