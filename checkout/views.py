ec5dfcb9956d0b006422b6045e05480044a55e61
CarylThom <cat.twnti@gmail.com>
2022-11-11 12:39:46 +0000

    Add order.email to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -175,14 +175,14 @@ def checkout_success(request, order_number):
         (
             'Thank you for your purchase from FitSpace!\n'
             'Your order details are as follows:\n\n'
-            f'Order Number: {order.order_number}\n'
+            f'Order Number: {order_number}\n'
             f'Grand Total: {order.grand_total}\n\n'
             'If you have any questions, please contact us at '
             f'{settings.DEFAULT_FROM_EMAIL}.\n\n'
             'Thanks for shopping at FitSpace'
         ),
         settings.DEFAULT_FROM_EMAIL,
-        ['email list'],
+        ['order.email'],
         fail_silently=False,
     )
 
