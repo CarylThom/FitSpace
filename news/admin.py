de1691246496b08af81607e635df63f1205562d9
CarylThom <cat.twnti@gmail.com>
2022-12-11 19:59:01 +0000

    Create model for news app

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post Admin"""
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
---
+++ news/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('articles/', views.PostList.as_view(), name='articles'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='article_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+]
