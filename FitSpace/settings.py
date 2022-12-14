661fdf61dbb70a88784d4ce9d926a3e96ff16fea
CarylThom <cat.twnti@gmail.com>
2022-12-14 21:52:51 +0000

    Create and set-up blog app

--- FitSpace/settings.py
+++ FitSpace/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'storages',
     'contact',
     'about',
-    'news',
+    'blog'
 ]
 
 MIDDLEWARE = [
--- blog/admin.py
+++ blog/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from .models import Post
 
 
-@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """Post Admin"""
-
     list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ("status",)
     search_fields = ['title', 'content']
-    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
+
+
+admin.site.register(Post, PostAdmin)
