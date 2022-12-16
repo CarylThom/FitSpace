2132aeacb6d3e762d67f6ea47496bd00075756ad
CarylThom <cat.twnti@gmail.com>
2022-12-16 21:01:33 +0000

    Make changes to newsletter model

--- newsletter/models.py
+++ newsletter/models.py
@@ -6,7 +6,6 @@ class Newsletter(models.Model):
 
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
