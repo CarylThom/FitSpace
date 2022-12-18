e2203fa124f6b9c1671b357350aa00f05dd12da8
CarylThom <cat.twnti@gmail.com>
2022-12-18 20:00:40 +0000

    Changes to newsletter html

--- newsletter/models.py
+++ newsletter/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Newsletter(models.Model):
     """ Newsletter fields """
 
-    full_name = models.CharField(max_length=75, blank=False, null=False)
+    name = models.CharField(max_length=75, blank=False, null=False)
     email = models.EmailField(max_length=256, blank=False, null=False)
 
     def __str__(self):
